03e820d84c22ade19aaff4f29c11e024604a32a2
Joseph <josephazzi2006@gmail.com>
2025-04-19 20:57:56 +0300

    Style for quiz

--- js/quizScript.js
+++ js/quizScript.js
@@ -3,15 +3,48 @@ function calculate(){
     var answer = [2,1,4,2,3,3,3,3,2,2,1,2,2,3,3,3,1,2,3,4];
     for(var i = 1 ; i <= 20 ; i++){
         var question = document.getElementById("qu"+i+"An"+answer[(i-1)]);
+        var allRadios = document.getElementsByName("qu"+i);
         if(question.checked){
             result++;
-            document.getElementById("question"+i).style.backgroundColor="lightgreen";
+            question.parentElement.parentElement.style.border="3px solid green";
         }else{
-            document.getElementById("question"+i).style.backgroundColor="#FFAAAA";
+            question.parentElement.parentElement.style.border="3px solid red";
+            for(var j = 0; j < allRadios.length; j++){
+                if(allRadios[j].checked){
+                    document.querySelector("label[for='" + allRadios[j].id + "']").style.color = "red";
+                }
+            }
+        }
+        document.querySelector("label[for='qu" + i + "An" + answer[(i-1)] + "']").style.color = "green";
+        for(var j = 0; j < allRadios.length; j++){
+            allRadios[j].disabled = true;
         }
     
     }
-    window.alert(result);
-    console.log(result);
+    document.getElementById('submit').style.display = "none";
+    document.getElementById('result').style.display = "flex";
+    document.getElementById('resultvalue').innerHTML = result;
+    window.scrollTo(0, 0);
+}
+
+function retake(){
+    document.getElementById('submit').style.display = "block";
+    document.getElementById('result').style.display = "none";
+    for(var i = 1; i <= 20; i++){
+        var allRadios = document.getElementsByName("qu"+i);
+        for(var j = 0; j < allRadios.length; j++){
+            allRadios[j].disabled = false;
+            allRadios[j].checked = false;
+            var label = document.querySelector("label[for='" + allRadios[j].id + "']");
+            if(label) {
+                label.style.color = "";
+            }
+        }
+        var questionContainer = document.getElementById("question"+i);
+        if(questionContainer) {
+            questionContainer.style.border = "";
+        }
+    }
     
+    window.scrollTo(0, 0);
 }
